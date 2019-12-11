1d8af79505bb499e37dae7f8580ad3a8fb7f98df
pgavriluk <pgavriluk@gmail.com>
2019-12-11 12:35:28 -0500

    first commit

---
+++ web/src/main/java/pavel/demo/util/ViewNames.java
@@ -0,0 +1,12 @@
+package pavel.demo.util;
+
+public final class ViewNames {
+
+	// constants
+	public static final String HOME = "home";
+	public static final String PLAY = "play";
+	public static final String GAME_OVER = "game-over";
+
+	// constructor
+	private ViewNames() {}
+}
